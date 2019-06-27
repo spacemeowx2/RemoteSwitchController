1988dd273bc494c755fdeffcb14562466b511b52
spacemeowx2 <spacemeowx2@gmail.com>
2019-06-28 04:29:50 +0800

    cli as script

--- splatoon-bot/src/cli.ts
+++ splatoon-bot/src/cli.ts
@@ -2,11 +2,36 @@ import { createInterface } from 'readline'
 import { ProController, ProUDPTransport, ButtonBitMap, PadButton } from './controller'
 import { delay } from './util'
 
+const StrBtnMap: Record<string, ButtonBitMap> = {
+    'w': ButtonBitMap.Up,
+    's': ButtonBitMap.Down,
+    'a': ButtonBitMap.Left,
+    'd': ButtonBitMap.Right,
+    'h': ButtonBitMap.Home,
+    'home': ButtonBitMap.Home,
+    'A': ButtonBitMap.A,
+    'b': ButtonBitMap.B,
+    'B': ButtonBitMap.B,
+    'x': ButtonBitMap.X,
+    'y': ButtonBitMap.Y,
+    'zl': ButtonBitMap.ZL,
+    'zr': ButtonBitMap.ZR,
+    'l': ButtonBitMap.L,
+    'r': ButtonBitMap.R,
+    '+': ButtonBitMap.Plus,
+    '-': ButtonBitMap.Minus,
+    'c': ButtonBitMap.Capture,
+    'capture': ButtonBitMap.Capture,
+}
+
 const DefaultPressDelay = 100
 
 function keyPresser(controller: ProController) {
     const { button } = controller
     return async(key: ButtonBitMap, delayTime = DefaultPressDelay) => {
+        if (isNaN(delayTime)) {
+            delayTime = DefaultPressDelay
+        }
         button.setKey(key, true)
         controller.send()
         await delay(delayTime)
@@ -18,8 +43,8 @@ function keyPresser(controller: ProController) {
 async function main (...argv: string[]) {
     const rl = createInterface({
         input: process.stdin,
-        output: process.stdout,
-        prompt: '>>> '
+        prompt: '>>> ',
+        crlfDelay: Infinity
     })
     if (argv.length === 0) {
         argv[0] = 'localhost:34952'
@@ -32,85 +57,64 @@ async function main (...argv: string[]) {
     const c = new ProController(new ProUDPTransport(argv[0]))
     const { button, leftStick } = c
     const press = keyPresser(c)
-    rl.prompt()
-    rl.on('line', async (line) => {
-        switch (line.trim()) {
-            case 'ctl':
-                await press(ButtonBitMap.A)
-                await delay(2000)
-                await Promise.all([
-                    press(ButtonBitMap.ZL),
-                    press(ButtonBitMap.ZR)
-                ])
-                await delay(1000)
-                await press(ButtonBitMap.A)
-                break
-            case 'w':
-                await press(ButtonBitMap.Up)
-                break
-            case 's':
-                await press(ButtonBitMap.Down)
-                break
-            case 'a':
-                await press(ButtonBitMap.Left)
-                break
-            case 'd':
-                await press(ButtonBitMap.Right)
-                break
-            case 'h':
-            case 'home':
-                await press(ButtonBitMap.Home)
-                break
-            case 'A':
-                await press(ButtonBitMap.A)
-                break
-            case 'b':
-            case 'B':
-                await press(ButtonBitMap.B)
-                break
-            case 'x':
-                await press(ButtonBitMap.X)
-                break
-            case 'y':
-                await press(ButtonBitMap.Y)
-                break
-            case 'zlr':
-                await Promise.all([
-                    press(ButtonBitMap.ZL),
-                    press(ButtonBitMap.ZR),
-                ])
-                break
-            case 'zl':
-                await press(ButtonBitMap.ZL)
-                break
-            case 'zl':
-                await press(ButtonBitMap.ZL)
-                break
-            case 'zr':
-                await press(ButtonBitMap.ZR)
-                break
-            case 'l':
-                await press(ButtonBitMap.L)
-                break
-            case 'r':
-                await press(ButtonBitMap.R)
-                break
-            case '+':
-                await press(ButtonBitMap.Plus)
-                break
-            case '-':
-                await press(ButtonBitMap.Minus)
-                break
-            case 'c':
-            case 'capture':
-                await press(ButtonBitMap.Capture)
-                break
+    const StrBtnKeys = Object.keys(StrBtnMap)
+    const handler = async (line: string) => {
+        let [cmd, ...params] = line.trim().split(',')
+        params = params.map(i => i.trim())
+        if (StrBtnKeys.includes(cmd)) {
+            const key = StrBtnMap[cmd]
+            await press(key)
+        } else {
+            switch (cmd) {
+                case 'ctl':
+                    await press(ButtonBitMap.A)
+                    await delay(2000)
+                    await Promise.all([
+                        press(ButtonBitMap.ZL),
+                        press(ButtonBitMap.ZR)
+                    ])
+                    await delay(1000)
+                    await press(ButtonBitMap.A)
+                    break
+                case 'sleep': {
+                    const ms = parseInt(params[0])
+                    await delay(ms)
+                    break
+                }
+                case 'down': {
+                    if (StrBtnKeys.includes(params[0])) {
+                        const key = StrBtnMap[params[0]]
+                        button.setKey(key, true)
+                        c.send()
+                    }
+                    break
+                }
+                case 'up': {
+                    if (StrBtnKeys.includes(params[0])) {
+                        const key = StrBtnMap[params[0]]
+                        button.setKey(key, false)
+                        c.send()
+                    }
+                    break
+                }
+                case 'reset':
+                    button.reset()
+                    c.send()
+                    break
+                case 'end':
+                    console.log('end')
+                    process.exit(0)
+            }
         }
+        console.log(line)
         rl.prompt()
-    }).on('close', () => {
-        console.log('bye')
-        process.exit()
-    })
+    }
+    let queue: Promise<void> = Promise.resolve()
+    rl.prompt()
+    rl
+        .on('line', line => {
+            queue = queue.then(() => handler(line))
+        })
 }
 
 main(...process.argv.slice(2)).catch(e => console.error(e))
